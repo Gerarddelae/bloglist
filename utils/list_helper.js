1a2310e00f0d38a3070d7fd7512f7123216c01fd
Gerardo <gerardmoe@hotmail.com>
2023-08-11 14:15:10 -0500

    unit testing step 3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,8 +8,21 @@ const totalLikes = (blogs) => {
   const array = blogs.map(blog => blog.likes)
   return array.reduce((a, b) => a + b, 0)
 }
+
+const favoriteBlog = (blogs) => {
+  const array = blogs.map(blog => blog.likes)
+  const max = Math.max(...array)
+  const index = array.indexOf(max)
+  const response = blogs[index]
+  return {
+    title: response.title,
+    author: response.author,
+    likes: response.likes
+  }
+}
   
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
