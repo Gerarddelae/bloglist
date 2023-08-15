0337bed7e2e30440b165fdf0e4ea3cc5aa4fd2b1
Gerardo <gerardmoe@hotmail.com>
2023-08-15 18:32:13 -0500

    bloglist test step 1

---
+++ tests/blog_api.test.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose")
+const supertest = require("supertest")
+const app = require("../app")
+const api = supertest(app)
+const Blog = require("../models/blog")
+const initialBlogs = [
+  {
+    title: "how to dance",
+    author: "MJ",
+    url: "https",
+    likes: 10
+  },
+  {
+    title: "how to cook",
+    author: "vives",
+    url: "httpss",
+    likes: 2
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
+
+test("blogs are returned as json", async () => {
+  await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/)
+})
+
+test("response have same length", async () => {
+  const response = await api.get("/api/blogs")
+
+  expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
